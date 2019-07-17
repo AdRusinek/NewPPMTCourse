34a93903f74286e3f29a1343851b711e40e8c341
AdRusinek <rusinekadrian19@gmail.com>
2019-07-17 02:59:08 +0200

    Added Validation Service

--- src/main/java/com/rusinek/ppmtool/web/ProjectController.java
+++ src/main/java/com/rusinek/ppmtool/web/ProjectController.java
@@ -1,14 +1,18 @@
 package com.rusinek.ppmtool.web;
 
 import com.rusinek.ppmtool.domain.Project;
+import com.rusinek.ppmtool.services.MapValidationErrorService;
 import com.rusinek.ppmtool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 /**
  * Created by Adrian Rusinek on 17.07.2019
@@ -20,9 +24,18 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private MapValidationErrorService errorService;
+
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project) {
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
+
+        ResponseEntity<?> errorMap = errorService.MapValidationService(result);
+        if (errorMap != null) return errorMap;
+
         projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
     }
+
+
 }
