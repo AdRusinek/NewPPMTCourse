f314bdfc0564f99615d9f9ce57873f2de1b37529
AdRusinek <rusinekadrian19@gmail.com>
2019-07-21 03:12:18 +0200

    Added react/redux support for all CRUD operations and now I started to implementing new Domain objects

---
+++ PPMToolFullStackBackend/src/main/java/com/rusinek/ppmtool/repositories/BacklogRepository.java
@@ -0,0 +1,9 @@
+package com.rusinek.ppmtool.repositories;
+
+import com.rusinek.ppmtool.domain.Backlog;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BacklogRepository extends CrudRepository<Backlog,Long> {
+}
